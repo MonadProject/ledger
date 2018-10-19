c1eb70f551eb20d3684333785a90c2a1fa8e2f89
xiaohuo <xiaohuo200@gmail.com>
2018-10-19 10:03:24 +0800

    deserialize fn

--- serialization/src/reader.rs
+++ serialization/src/reader.rs
@@ -7,16 +7,27 @@ pub struct Reader<T> {
     inner: T
 }
 
-pub enum Error {}
+pub enum Error {
+    ReadDataErr
+}
 
 pub trait Deserializable {
     fn deserialize<T>(reader: &mut Reader<T>) -> Result<Self, Error> where Self: Sized, T: io::Read;
 }
 
-impl<T> io::Read for Reader<T> where T: io::Read {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
-        io::Read::read(&mut self.inner, buf)
+
+impl<R> Reader<R> where R: io::Read {
+    pub fn from_buffer(buffer: R) -> Self {
+        Reader {
+            inner: buffer
+        }
     }
+
+    pub fn read<T>(&mut self) -> Result<T, Error> where T: Deserializable {
+        T::deserialize(self)
+    }
+
+
 }
 
 
@@ -26,3 +37,23 @@ impl Deserializable for u8 {
     }
 }
 
+pub fn deserialize<R, T>(buffer: R) -> Result<T, Error> where R: io::Read, T: Deserializable {
+    let mut reader = Reader::from_buffer(buffer);
+    reader.read::<T>()
+}
+
+
+// 为了得到 ReadBytesExt 的能力
+impl<T> io::Read for Reader<T> where T: io::Read {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
+        io::Read::read(&mut self.inner, buf)
+    }
+}
+
+impl<'a> Reader<&'a [u8]> {
+    fn from_bytes(bytes: &'a [u8]) -> Self {
+        Reader {
+            inner: bytes
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let mut x = 5;
-    let y = &x;
-    println!("{}",*y)
+    let r = f1(String::from("aa"), 1u8);
+    println!("{}", r)
+}
+
+
+fn f1<A, B>(a: A, b: B) -> B {
+    f2(b)
+}
+
+fn f2<A>(b: A) -> A {
+    b
 }
