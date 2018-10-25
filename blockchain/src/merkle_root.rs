3d1ac2032bc5035e145c37eadf165395971b71e6
xiaohuo <xiaohuo200@gmail.com>
2018-10-25 11:51:22 +0800

    develop merkle root

--- blockchain/src/merkle_root.rs
+++ blockchain/src/merkle_root.rs
@@ -1,5 +1,5 @@
 use basictype::hash::{Hash256, Hash512};
-use crypto::double_sha256;
+use crypto::double_sha2;
 
 fn concatenate<T>(a: T, b: T) -> Hash512 where T: AsRef<Hash256> {
     let mut h521 = Hash512::default();
@@ -8,6 +8,27 @@ fn concatenate<T>(a: T, b: T) -> Hash512 where T: AsRef<Hash256> {
     h521
 }
 
-//fn calculate_merge_hash<T>(a: T, b:T) -> Hash256 where T: AsRef<Hash256> {
-//
-//}
+fn calculate_merge_hash<T>(a: T, b: T) -> Hash256 where T: AsRef<Hash256> {
+    double_sha2(&*concatenate(a, b))
+}
+
+// see https://en.bitcoin.it/wiki/Protocol_documentation#Merkle_Trees
+fn calculate_merge_root<T>(input: &[T]) -> Hash256 where T: AsRef<Hash256> {
+    if input.len() == 1 {
+        return input[0].as_ref().clone();
+    }
+    let mut vec = Vec::new();
+    let index = 0;
+    while index + 1 < input.len() {
+        //a little weird，never mind
+        let node = calculate_merge_hash(&input[index], &input[index + 1]);
+        vec.push(node);
+    }
+
+    if input.len() % 2 != 0 {
+        let node = calculate_merge_hash(&input[input.len() - 1], &input[input.len() - 1]);
+        vec.push(node);
+    }
+
+    calculate_merge_root(&vec)
+}
