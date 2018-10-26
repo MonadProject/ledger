5dd00b1fe03d55bead4bb98cf15ff0d293a825f5
xiaohuo <xiaohuo200@gmail.com>
2018-10-26 13:36:51 +0800

    try start server

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -2,8 +2,10 @@ extern crate futures;
 extern crate futures_cpupool;
 extern crate tokio_io;
 extern crate tokio_core;
+extern crate tokio;
 
 
 mod context;
 mod socket;
 mod p2p;
+mod event_loop;
--- p2p/src/p2p.rs
+++ p2p/src/p2p.rs
@@ -1,1 +1,34 @@
-use tokio_core::net::{TcpListener,TcpStream};
+use futures::stream::Stream;
+use std::net::SocketAddr;
+use tokio::io;
+use tokio::prelude::*;
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::{Handle, Interval, Remote, Timeout};
+use tokio_io::io::{read_exact, ReadExact};
+
+fn start_server(address: SocketAddr, handle: &Handle) {
+    let socket = TcpListener::bind(&address, handle).unwrap();
+    println!("Listening on: {}", address);
+    let server = socket.incoming()
+        .map_err(|e| println!("failed to accept socket; error = {:?}", e))
+        .for_each(move |socket| {
+            Ok(())
+        });
+    handle.spawn(server);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use event_loop;
+    use std::net::SocketAddr;
+    use super::start_server;
+
+    #[test]
+    fn test_start_serer() {
+        let address = "127.0.0.1:8080".parse::<SocketAddr>().unwrap();
+        let mut el = event_loop::event_loop();
+        start_server(address, &el.handle());
+        el.run(event_loop::forever()).unwrap();
+    }
+}
