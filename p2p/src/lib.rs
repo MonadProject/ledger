243b46d7d1e95bb19b8755ed564ef0c5043ffc07
xiaohuo <xiaohuo200@gmail.com>
2018-10-26 14:13:31 +0800

    Init new mod: async io

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -1,11 +1,12 @@
 extern crate futures;
 extern crate futures_cpupool;
-extern crate tokio_io;
-extern crate tokio_core;
 extern crate tokio;
+extern crate tokio_core;
+extern crate tokio_io;
 
 
 mod context;
 mod socket;
 mod p2p;
 mod event_loop;
+mod async_io;
