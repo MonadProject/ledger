7b1f959036218bd42e1ddb31f3463f0a53ede141
xiaohuo <xiaohuo200@gmail.com>
2018-10-27 12:27:21 +0800

    serialize for message header, test it

--- protocol/src/message/message_header.rs
+++ protocol/src/message/message_header.rs
@@ -28,3 +28,27 @@ impl Serializable for MessageHeader {
         unimplemented!()
     }
 }
+
+#[cfg(test)] //cargo test -- --nocapture
+mod tests {
+    use super::Command;
+    use super::Hash32;
+    use super::MessageHeader;
+    use super::Stream;
+    use serialization::stream::Serializable;
+
+    #[test]
+    fn test_serialize() {
+        let header = MessageHeader {
+            magic: 1u32,
+            command: Command::from_hex_string("76657273696f6e0000000000"),
+            payload_size: 4u32,
+            checksum: Hash32::from("6f6e0000"),
+        };
+        let mut stream = Stream::new();
+
+        header.serialize(&mut stream);
+
+        println!("{:?}",stream);
+    }
+}
