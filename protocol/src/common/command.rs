939857d59ff89a22d71b26a33965bc43bbb3efb4
xiaohuo <xiaohuo200@gmail.com>
2018-10-27 12:18:00 +0800

    add from hex string for command

--- protocol/src/common/command.rs
+++ protocol/src/common/command.rs
@@ -1,9 +1,11 @@
 use basictype::hash::Hash96;
 use serialization::reader::Deserializable;
-use serialization::stream::{Serializable, Stream};
-use serialization::reader::Reader;
 use serialization::reader::Error;
+use serialization::reader::Reader;
+use serialization::stream::{Serializable, Stream};
 use std::io;
+use std::str::FromStr;
+
 
 #[derive(Debug)]
 pub struct Command(Hash96);
@@ -19,21 +21,35 @@ impl Serializable for Command {
 }
 
 impl Deserializable for Command {
-    fn deserialize<T>(reader: & mut Reader<T>) -> Result<Self, Error> where Self: Sized, T: io::Read {
+    fn deserialize<T>(reader: &mut Reader<T>) -> Result<Self, Error> where Self: Sized, T: io::Read {
         let hash = reader.read::<Hash96>().unwrap();
         Ok(Command(hash))
     }
 }
 
+impl Command {
+    pub fn from_hex_string(string: &str) -> Self {
+        let hash = Hash96::from_str(&string).unwrap();
+        Command(hash)
+    }
+}
+
 #[cfg(test)] //cargo test -- --nocapture
 mod tests {
-    use serialization::stream::Serializable;
     use serialization::reader::Deserializable;
+    use serialization::stream::Serializable;
     use std::str::FromStr;
     use super::Command;
     use super::Hash96;
-    use super::Stream;
     use super::Reader;
+    use super::Stream;
+
+    #[test]
+    fn test_from_hex_string() {
+        let string = String::from("76657273696f6e0000000000");
+        let command = Command::from_hex_string(&string);
+        println!("{:?}",command);
+    }
 
     #[test]
     fn test_serialize() {
@@ -50,7 +66,6 @@ mod tests {
         let buffer = [118u8, 101, 114, 115, 105, 111, 110, 0, 0, 0, 0, 0];
         let mut reader = Reader::from_bytes(&buffer);
         let result = Command::deserialize(&mut reader).unwrap();
-        println!("{:?}",result);
-
+        println!("{:?}", result);
     }
 }
