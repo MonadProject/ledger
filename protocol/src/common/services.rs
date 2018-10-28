006cb28b02d3c3d5521b545bdd8e61aefa69e81d
xiaohuo <xiaohuo200@gmail.com>
2018-10-28 15:30:07 +0800

    impl ops deref for Services, and test it

--- protocol/src/common/services.rs
+++ protocol/src/common/services.rs
@@ -1,13 +1,36 @@
 use serialization::stream::{Serializable, Stream};
+use std::ops;
+use std::ops::Deref;
 
 pub struct Services(u64);
 
 impl Serializable for Services {
     fn serialize(&self, s: &mut Stream) {
-        s.write(&self.0)
+        s.write(&self.0);
     }
 
     fn serialized_size(&self) -> usize {
         8
     }
 }
+
+impl ops::Deref for Services {
+    type Target = u64;
+
+    fn deref(&self) -> &<Self as Deref>::Target {
+        &self.0
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Services;
+
+    #[test]
+    fn test_deref() {
+        let services = Services(1u64);
+        assert_eq!(1u64, *services);
+    }
+}
+
