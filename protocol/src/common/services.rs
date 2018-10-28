a9e87a2c0896f2ad9f607f1c122c140ef17f3b1f
xiaohuo <xiaohuo200@gmail.com>
2018-10-28 15:24:20 +0800

    impl serializable for services

--- protocol/src/common/services.rs
+++ protocol/src/common/services.rs
@@ -1,1 +1,13 @@
+use serialization::stream::{Serializable, Stream};
+
 pub struct Services(u64);
+
+impl Serializable for Services {
+    fn serialize(&self, s: &mut Stream) {
+        s.write(&self.0)
+    }
+
+    fn serialized_size(&self) -> usize {
+        8
+    }
+}
