63b25d1de9d15780ba59ddfb17a4165d4483eb17
xiaohuo <xiaohuo200@gmail.com>
2018-11-02 16:54:33 +0800

    first test monad serializable, it seems work :0))))0

--- blockchain/src/block_header.rs
+++ blockchain/src/block_header.rs
@@ -12,3 +12,30 @@ pub struct BlockHeader {
     pub n_bits: u32,
     pub nonce: u32,
 }
+
+
+#[cfg(test)] //cargo test -- --nocapture
+mod tests {
+    use basictype::hash;
+    use super::BlockHeader;
+    use serialization::stream::{Serializable,Stream};
+
+    #[test]
+    fn test_serialize_with_annotation() {
+
+        let block_header = BlockHeader {
+            version: 1,
+            previous_block_header_hash: hash::Hash256::from_reversed_string("0000000000000000000383fb0c96397da185a378d04cf7d451ef81a7b446fbb7"),
+            merkle_root_hash: hash::Hash256::from_reversed_string("0000000000000000000383fb0c96397da185a378d04cf7d451ef81a7b446fbb7"),
+            time: 0,
+            n_bits: 0,
+            nonce: 0,
+        };
+
+        let mut stream = Stream::new();
+
+        block_header.serialize(&mut stream);
+
+        println!("{:?}",stream);
+    }
+}
